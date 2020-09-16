549a5d12fe288b69cc751b8085c6aa5ab22bf4ae
jamvo <j.stojicik@gmail.com>
2020-09-16 19:52:11 +0200

    minor improvements to WordCounter. eliminating duplicates.

--- WordCounter/src/main/java/Application.java
+++ WordCounter/src/main/java/Application.java
@@ -4,21 +4,22 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 
 public class Application {
     public static void main(String[] args) throws IOException {
         int count = 0;
         File file = new File("src/file.txt");
-        String fileString = FileUtils.readFileToString(file, "UTF-8").toLowerCase().replaceAll(",", "").replaceAll("[.]", "");
+        String fileString = FileUtils.readFileToString(file, "UTF-8").toLowerCase().replaceAll("[^a-zA-Z ]", "");
         ArrayList<String> stringList = new ArrayList<>(Arrays.asList(fileString.split(" ")));
-        ArrayList<String> stringsCountedList = new ArrayList<>();
+        LinkedHashSet<String> stringsCountedList = new LinkedHashSet<>();
         for (String s : stringList) {
             for (String s1 : stringList) {
                 if (s.equals(s1)) {
                     count++;
                 }
             }
-            stringsCountedList.add(s + " " + count);
+            stringsCountedList.add("<" + s + ">" + " : " + "<" + count + ">");
             count = 0;
         }
         stringsCountedList.forEach(System.out::println);
