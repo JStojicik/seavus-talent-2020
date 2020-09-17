e39a983181df81a7794d454a74ea7f75481162fd
jamvo <j.stojicik@gmail.com>
2020-09-17 17:12:39 +0200

    Removed utility package and Util class. placed the charCounter method in the Application class. Also changed the logic int he method to count all the characters in the entered sentance.

--- CharCounter/src/com/company/Application.java
+++ CharCounter/src/com/company/Application.java
@@ -1,8 +1,9 @@
 package com.company;
 
-import com.company.utility.Util;
-
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Application {
 
@@ -10,9 +11,26 @@ public class Application {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a sentence");
         String text = sc.nextLine();
-        System.out.println("enter a character to check how many times it shows up in your sentence. Enter 0 to exit.\n If you enter more than 1 character the first will be taken!");
-        String chr = sc.nextLine();
-        int count = Util.charCounter(text.toLowerCase(), chr.toLowerCase());
-        System.out.println("The character " + chr.charAt(0) + " is found " + count + " times in the sentence");
+        System.out.println("The characters printed with the number of apearences in the sentance you entered");
+        charCounter(text.toLowerCase()).forEach(System.out::println);
+    }
+
+    static Set<String> charCounter(String text) {
+        ArrayList<Character> charList = new ArrayList<>();
+        LinkedHashSet<String> countedCharList = new LinkedHashSet<>();
+        for (int i = 0; i < text.length(); i++) {
+            charList.add(text.charAt(i));
+        }
+        for (Character c : charList) {
+            int count = 0;
+            for (Character c1 : charList) {
+                if (c.equals(c1)) {
+                    count++;
+                }
+            }
+            countedCharList.add(c + " : " + count);
+
+        }
+        return countedCharList;
     }
 }
