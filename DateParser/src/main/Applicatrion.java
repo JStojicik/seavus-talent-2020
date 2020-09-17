7516b0f2381c06e72ee2bbfb9cfa99597f003ecd
jamvo <j.stojicik@gmail.com>
2020-09-17 17:43:04 +0200

    Removed utility package and Util class. Implemented the dateList method in the Application class

--- DateParser/src/main/Applicatrion.java
+++ DateParser/src/main/Applicatrion.java
@@ -1,7 +1,7 @@
 package main;
 
-import utility.Util;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Applicatrion {
@@ -10,6 +10,17 @@ public class Applicatrion {
         System.out.println("Enter a date in the format 'MM/DD/YYYY' such as '12/04/2007'");
         String date = sc.nextLine();
         System.out.println("The date you have entered is: ");
-        Util.dateList(date);
+        dateList(date);
+    }
+
+    static void dateList(String date) {
+        ArrayList<String> dateList = new ArrayList<>(Arrays.asList(date.split("/")));
+        for (int i = 0; i < dateList.size(); i++) {
+            switch (i) {
+                case 0 -> System.out.println("Month: " + dateList.get(i));
+                case 1 -> System.out.println("Day: " + dateList.get(i));
+                case 2 -> System.out.println("Year: " + dateList.get(i));
+            }
+        }
     }
 }
