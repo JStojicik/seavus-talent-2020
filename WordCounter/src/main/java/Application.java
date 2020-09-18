55bc1d090edf4c605fdd1805b50efad9301de261
jamvo <j.stojicik@gmail.com>
2020-09-18 12:07:26 +0200

    Moving declaration and initialization of count in foreach loop

--- WordCounter/src/main/java/Application.java
+++ WordCounter/src/main/java/Application.java
@@ -8,19 +8,19 @@ import java.util.LinkedHashSet;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        int count = 0;
+
         File file = new File("src/file.txt");
         String fileString = FileUtils.readFileToString(file, "UTF-8").toLowerCase().replaceAll("[^a-zA-Z ]", "");
         ArrayList<String> stringList = new ArrayList<>(Arrays.asList(fileString.split(" ")));
         LinkedHashSet<String> stringsCountedSet = new LinkedHashSet<>();
         for (String s : stringList) {
+            int count = 0;
             for (String s1 : stringList) {
                 if (s.equals(s1)) {
                     count++;
                 }
             }
             stringsCountedSet.add("<" + s + ">" + " : " + "<" + count + ">");
-            count = 0;
         }
         stringsCountedSet.forEach(System.out::println);
     }
