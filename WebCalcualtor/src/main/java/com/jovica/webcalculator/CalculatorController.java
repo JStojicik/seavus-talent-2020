86ad8bf910349777ede948c879f684183bd76bf4
jamvo <j.stojicik@gmail.com>
2020-09-20 21:37:29 +0200

    Giving integer properties suitable names

--- WebCalcualtor/src/main/java/com/jovica/webcalculator/CalculatorController.java
+++ WebCalcualtor/src/main/java/com/jovica/webcalculator/CalculatorController.java
@@ -14,11 +14,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class CalculatorController {
     @RequestMapping(path = "/calculator", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public String calculate(@RequestParam int i1, @RequestParam int i2, @RequestParam String operator) {
+    public String calculate(@RequestParam int operandOne, @RequestParam int operandTwo, @RequestParam String operator) {
         if (operator.equals("add")) {
-            return "The addition of the two numbers is " + (i1 + i2);
+            return "The addition of the two numbers is " + (operandOne + operandTwo);
         } else if (operator.equals("sub")) {
-            return "The subtraction of the two numbers is " + (i1 - i2);
+            return "The subtraction of the two numbers is " + (operandOne - operandTwo);
         } else {
             return "no such operator";
         }
