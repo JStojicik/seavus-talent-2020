db7fdc2b1000c61b992334b32e91b8f654aefbad
jamvo <j.stojicik@gmail.com>
2020-09-23 15:04:40 +0200

    Made the application more dynamic with removing the list fileStringList and using Arrays.stream()

--- WordSorterWithLambdaAndFilter/src/main/java/Application.java
+++ WordSorterWithLambdaAndFilter/src/main/java/Application.java
@@ -2,7 +2,6 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
@@ -15,7 +14,6 @@ public class Application {
     public static void main(String[] args) throws IOException {
         File file = new File("src/file.txt");
         String fileString = FileUtils.readFileToString(file, "UTF-8").toLowerCase().replaceAll("[^a-zA-Z ]", "");
-        ArrayList<String> stringList = new ArrayList<>(Arrays.asList(fileString.split(" ")));
         Scanner sc = new Scanner(System.in);
         System.out.println("choose the sorting you like for the list of words contained in the file file.txt\n(desc)for descending\n(asc)for ascending");
         String sortType = sc.nextLine();
@@ -25,9 +23,17 @@ public class Application {
             System.out.println("choose the maximum length of a word");
             int maxLength = Integer.parseInt(sc.nextLine());
             if (sortType.toLowerCase().equals(ASCENDING)) {
-                stringList.stream().sorted(Comparator.comparingInt(String::length)).filter(s -> maxLength > 0 ? (s.length() <= maxLength && s.length() >= minLength) : s.length() >= minLength).distinct().forEach(System.out::println);
+                Arrays.stream(fileString.split(" "))
+                        .sorted(Comparator.comparingInt(String::length))
+                        .filter(s -> maxLength > 0 ? (s.length() <= maxLength && s.length() >= minLength) : s.length() >= minLength)
+                        .distinct()
+                        .forEach(System.out::println);
             } else if (sortType.toLowerCase().equals(DESCENDING)) {
-                stringList.stream().sorted((s1, s2) -> s2.length() - s1.length()).filter(s -> maxLength > 0 ? (s.length() <= maxLength && s.length() >= minLength) : s.length() >= maxLength).distinct().forEach(System.out::println);
+                Arrays.stream(fileString.split(" "))
+                        .sorted((s1, s2) -> s2.length() - s1.length())
+                        .filter(s -> maxLength <= 0 || s.length() <= maxLength && s.length() >= minLength)
+                        .distinct()
+                        .forEach(System.out::println);
             } else {
                 System.out.println("invalid sort type");
             }
